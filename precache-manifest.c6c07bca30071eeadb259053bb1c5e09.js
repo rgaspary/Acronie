a8145969d13ddcacf8e1a129d28b9c262c3e8cd2
Renzo Gaspary <me@renzogaspary.com>
2020-07-15 22:23:09 -0400

    Updates

---
+++ precache-manifest.c6c07bca30071eeadb259053bb1c5e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afa285a1f917c16599ced7e05aaa149",
+    "url": "/Acronie/index.html"
+  },
+  {
+    "revision": "1e1725df37ac26965cbf",
+    "url": "/Acronie/static/css/main.7bd6ed9c.chunk.css"
+  },
+  {
+    "revision": "b9f07cac0cc8d41d8231",
+    "url": "/Acronie/static/js/2.edba8ddd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Acronie/static/js/2.edba8ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1725df37ac26965cbf",
+    "url": "/Acronie/static/js/main.2a5b6c3d.chunk.js"
+  },
+  {
+    "revision": "84c37798d92e24017953",
+    "url": "/Acronie/static/js/runtime-main.e24527d1.js"
+  }
+]);
